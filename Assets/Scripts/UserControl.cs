8910f07b00fd599aa3748041611c6c6fc72ddcf8
chishige1217200 <daichicchi01@gmail.com>
2023-12-15 19:52:52 +0900

    Add: 再生，停止，一時停止，音量調整実装

--- Assets/Scripts/UserControl.cs
+++ Assets/Scripts/UserControl.cs
@@ -10,12 +10,104 @@ public class UserControl : MonoBehaviour
 {
     [SerializeField] string testPath;
     [SerializeField] AudioSource audioSource;
+    [SerializeField] Button playButton;
+    [SerializeField] Button pauseButton;
+    [SerializeField] Button stopButton;
+    [SerializeField] Button loopButton;
+    [SerializeField] Slider timeSlider;
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Text timeText;
+    [SerializeField] Text volumeText;
+    [SerializeField] InputField pathField;
     bool doLoop = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        volumeSlider.value = audioSource.volume * 100f;
+        volumeText.text = volumeSlider.value.ToString();
         LoadWav(testPath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(audioSource.clip != null && audioSource.isPlaying)
+        {
+            UpdateTime();
+        }
+    }
+
+    void UpdateTime()
+    {
+        if (audioSource.clip != null)
+        {
+            // 全体時間算出
+            int a_minutes = (int)(audioSource.clip.length / 60);
+            int a_seconds = (int)(audioSource.clip.length % 60);
+            // 現在時間算出
+            int p_minutes = (int)(audioSource.time / 60);
+            int p_seconds = (int)(audioSource.time % 60);
+
+            // 時間表示更新
+            string timetext = p_minutes.ToString("D2") + ":" + p_seconds.ToString("D2") + " / " + a_minutes.ToString("D2") + ":" + a_seconds.ToString("D2");
+            timeText.text = timetext;
+
+            // スライダー更新
+            timeSlider.value = audioSource.time / audioSource.clip.length;
+        }
+    }
+
+    public void ResetUI()
+    {
+        playButton.gameObject.SetActive(true);
+        pauseButton.gameObject.SetActive(false);
+        UpdateTime();
+        timeSlider.value = 0;
+    }
+
+    public void PlayButton_Click()
+    {
+        if (audioSource.clip != null)
+        {
+            playButton.gameObject.SetActive(false);
+            pauseButton.gameObject.SetActive(true);
+
+            if(audioSource.time == 0)
+            {
+                audioSource.Play();
+                return;
+            }
+
+            audioSource.UnPause();
+        }
+    }
+
+    public void PauseButton_Click()
+    {
+        if (audioSource.clip != null)
+        {
+            pauseButton.gameObject.SetActive(false);
+            playButton.gameObject.SetActive(true);
+
+            audioSource.Pause();
+            // UpdateTime();
+        }
+    }
+
+    public void StopButton_Click()
+    {
+        if (audioSource.clip != null)
+        {
+            audioSource.Stop();
+            ResetUI();
+        }
+    }
+
+    public void SetVolume()
+    {
+        audioSource.volume = volumeSlider.value / 100f;
+        volumeText.text = volumeSlider.value.ToString();
     }
 
     public void LoadWav(string wavPath)
@@ -49,7 +141,8 @@ public class UserControl : MonoBehaviour
             if (audioSource.clip != null)
             {
                 audioSource.Stop();
-                // audioSource.clip = null;
+                ResetUI();
+                timeText.text = "00:00 / 00:00";
             }
             StartCoroutine(LoadWavCoroutine(wavPath));
         }
